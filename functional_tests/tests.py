c76e3e67000d627666f39d2d82976e1388484d83
=Paivi <paivi.e.suomela@gmail.com>
2015-03-26 11:51:07 +0000

    added new url, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -60,8 +60,8 @@ class NewVisitorTest(LiveServerTestCase):
    
     # Now a new user, Francis, comes along to the site.
 
-    # We use a new browser session to ensure that no info of Edith's is
-    # coming through from cookies etc
+    ## We use a new browser session to ensure that no info of Edith's is
+    ## coming through from cookies etc
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
--- lists/tests.py
+++ lists/tests.py
@@ -36,7 +36,12 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
 
 class ItemModelTest(TestCase):
 
@@ -58,19 +63,23 @@ class ItemModelTest(TestCase):
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item teh second')
         
-    def test_home_page_only_saves_items_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        self.assertEqual(Item.objects.count(), 0)
 
-    def test_home_page_displays_all_list_items(self):
+
+
+class ListViewTest(TestCase):
+    
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'lists/list.html')
+
+    def test_displays_all_items(self):
         Item.objects.create(text="item 1")
         Item.objects.create(text='item 2')
 
-        request = HttpRequest()
-        response = home_page(request)
+        response = self.client.get('/lists/the-only-list-in-the-world/')
 
-        self.assertIn('item 1', response.content.decode())
-        self.assertIn('item 2', response.content.decode())
+        self.assertContains(response, 'item 1')
+        self.assertContains(response, 'item 2')
+
 
 
